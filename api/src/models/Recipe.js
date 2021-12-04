b885c5e4e7587ca754e0b3d292f63579e65f131c
Abdias Jimenez <abdiasejr@gmail.com>
2021-12-03 21:48:14 -0300

    adding new attributes to recipe model

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -12,7 +12,7 @@ module.exports = (sequelize) => {
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
       },
-      recipeName: {
+      title: {
         type: DataTypes.STRING,
         allowNull: false,
       },
